2dd02bb781356db2e67149fe950a01a4647c5c82
Christopher Chu <christopherinsup@gmail.com>
2019-06-06 14:34:30 -0700

    Add testAuthority

--- projects/chuch/FinalProject/URLValidatorInCorrect/test/randomTest.java
+++ projects/chuch/FinalProject/URLValidatorInCorrect/test/randomTest.java
@@ -56,7 +56,46 @@ public class randomTest extends TestCase {
         //assertFalse(urlVal.isValid("hstps://www.google.com"));
     }
 
-    public void test() {
+    public void testAuthority() {
+        UrlValidator urlValTwo = new UrlValidator(UrlValidator.ALLOW_LOCAL_URLS);
+        String[] validPorts = {":80",":32562",":45802", ":230", ":23456"};
+        String[] invalidPorts = {":-23", ":67235", ":65600", ":-80", ":-1"};
+        String[] defaultSchemes = {"http", "https", "ftp"};
+
+        //authority can include the hostname and port
+        //assertTrue(urlValTwo.isValidAuthority("localhost:65600"));
+
+        //iterate through 10 validPorts and invalidPorts
+        for(int i=0; i < 10; i++){
+            int randomPort = (int)(Math.random() * 4 + 0);
+
+            //combine host and port strings
+            String auth = "localhost" + validPorts[randomPort];
+            String invalidAuth = "localhost" + invalidPorts[randomPort];
+
+            //tests to see if randomized valid and randomized invalidPorts pass tests
+            //use isValidAuthority method
+            assertTrue(urlValTwo.isValidAuthority(auth));
+            assertFalse(urlValTwo.isValidAuthority(invalidAuth));
+
+        }
+        //iterate through 10 urls including the schemes with localhost and validPorts and call isValid
+        for(int i=0; i < 10; i++){
+            int randomPort = (int)(Math.random() * 4 + 0);
+            int randomSch = (int)(Math.random() * 2 + 0);
+
+            //combined components for URL
+            String url = defaultSchemes[randomSch] + "://localhost" + validPorts[randomPort];
+            String urlTwo = defaultSchemes[randomSch] + "://localhost" + invalidPorts[randomPort];
+
+            //call isValid
+            assertTrue(urlValTwo.isValid(url));
+            assertFalse(urlValTwo.isValid(urlTwo));
+
+        }
+
     }
+
+
 }
 
